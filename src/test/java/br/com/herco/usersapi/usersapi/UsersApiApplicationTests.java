f83d2e4a9bd046104d584797e7fa4f376ae95405
HerculesGit <hercules.silva@dce.ufpb.br>
2019-11-04 13:45:18 -0300

    first commmit

---
+++ src/test/java/br/com/herco/usersapi/usersapi/UsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herco.usersapi.usersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
